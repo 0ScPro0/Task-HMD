e648acd81d9ba3c36da7ddf402a8634f50b68862
ScPro <muhamadievbogdan@gmai.com>
2026-04-07 20:43:24 +0500

    change in __init__.py file in database

--- backend/src/database/__init__.py
+++ backend/src/database/__init__.py
@@ -1,3 +1,20 @@
 from database.database import database
 
-__all__ = ["database"]
+from database.models.user import User
+from database.models.request import Request
+from database.models.notification import Notification
+from database.models.news import News
+
+from database.types.user import UserRole
+from database.types.request import RequestStatus, RequestType
+
+__all__ = [
+    "database",
+    "User",
+    "UserRole",
+    "Request",
+    "RequestStatus",
+    "RequestType",
+    "Notification",
+    "News",
+]
