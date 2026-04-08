cf1bc14d4d6f12996d8bbe43ea3998efbff1c686
ScPro <muhamadievbogdan@gmai.com>
2026-04-08 23:48:21 +0500

    change in service/__init__.py file

--- backend/src/services/__init__.py
+++ backend/src/services/__init__.py
@@ -1,4 +1,13 @@
 from services.auth import AuthService
 from services.user import UserService
+from services.request import RequestService
+from services.notification import NotificationService
+from services.news import NewsService
 
-__all__ = ["AuthService", "UserService"]
+__all__ = [
+    "AuthService",
+    "UserService",
+    "RequestService",
+    "NotificationService",
+    "NewsService",
+]
