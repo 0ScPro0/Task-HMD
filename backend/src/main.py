7b7a032ba4ad2d5847c71f684a2f9473c2696c1c
ScPro <muhamadievbogdan@gmai.com>
2026-04-09 20:11:00 +0500

    update main.py file

--- backend/src/main.py
+++ backend/src/main.py
@@ -1,11 +1,13 @@
 from contextlib import asynccontextmanager
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from sqladmin import Admin
 import uvicorn
 
 from core.config import settings
 from api.v1.router import router
 from database import database
+from admin import setup_admin
 
 
 @asynccontextmanager
@@ -25,6 +27,8 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
+setup_admin(app, True)
 
 if __name__ == "__main__":
     uvicorn.run(
