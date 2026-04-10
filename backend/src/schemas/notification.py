9642ba4d0a1ddd900aa19606d76a49c8ed9fd4aa
ScPro <muhamadievbogdan@gmai.com>
2026-04-11 04:22:37 +0500

    update user_notification schemas

--- backend/src/schemas/notification.py
+++ backend/src/schemas/notification.py
@@ -2,6 +2,8 @@ from typing import Optional
 from pydantic import BaseModel, ConfigDict, Field
 from datetime import datetime
 
+from schemas.user_notification import UserNotificationResponse
+
 
 class NotificationBase(BaseModel):
     title: str = Field(min_length=3, max_length=200)
@@ -27,3 +29,7 @@ class NotificationResponse(NotificationBase):
     updated_at: datetime
 
     model_config = ConfigDict(from_attributes=True)
+
+
+class FullNotificationResponse(NotificationResponse, UserNotificationResponse):
+    pass
