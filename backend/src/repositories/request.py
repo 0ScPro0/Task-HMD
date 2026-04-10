f7f537beb3136011d96d3f29aa4cd6ff9eef8946
ScPro <muhamadievbogdan@gmai.com>
2026-04-10 22:12:47 +0500

    update request repository

--- backend/src/repositories/request.py
+++ backend/src/repositories/request.py
@@ -30,6 +30,103 @@ class RequestRepository(BaseRepository[Request, RequestCreate, RequestUpdate]):
         request = await self.get(session, id=request_id)
         return request
 
+    async def get_requests_by_user(
+        self, session: AsyncSession, *, user_id: int, limit: Optional[int] = 100
+    ) -> Optional[List[Request]]:
+        """
+        Get Requests list by user id
+
+        Args:
+            session: Database session
+            user_id: Specific user id
+            limit: Limit of entry
+
+        Returns:
+            List of requests or None if not found
+        """
+        requests = await self.get_by_field_many(
+            self, session, field_name="user_id", field_value=user_id, limit=limit
+        )
+        return requests
+
+    async def get_request_status(self, session: AsyncSession, request_id: int) -> str:
+        """
+        Get Request status
+
+        Args:
+            session: Database session
+            request_id: Specific request id
+
+        Returns:
+            Request status
+        """
+        request = await self.get_request(session, request_id=request_id)
+        return request.status
+
+    async def update_request_executor(
+        self, session: AsyncSession, request_id: int, executor_id: int
+    ) -> Request:
+        """
+        Update Request executor
+
+        Args:
+            session: Database session
+            request_id: Specific request id
+            executor_id: Specific executor id
+
+        Returns:
+            Updated Request object
+        """
+        updated_request = await self.update_field(
+            session,
+            object_id=request_id,
+            field_name="executor_id",
+            field_value=executor_id,
+        )
+        return updated_request
+
+    async def update_request_status(
+        self, session: AsyncSession, *, request_id: int, status: str
+    ) -> Request:
+        """
+        Update Request status
+
+        Args:
+            session: Database session
+            request_id: Specific request id
+            status: New status
+
+        Returns:
+            Updated Request object
+        """
+        updated_request = await self.update_field(
+            session, object_id=request_id, field_name="status", field_value=status
+        )
+        return updated_request
+
+    async def update_request_executor_and_status(
+        self, session: AsyncSession, request_id: int, executor_id: int, status: str
+    ) -> Request:
+        """
+        Update Request executor and status
+
+        Args:
+            session: Database session
+            request_id: Specific request id
+            executor_id: Specific executor id
+            status: Request status
+
+        Returns:
+            Updated Request object
+        """
+        fields = {"executor_id": executor_id, "status": status}
+        updated_request = await self.update_fields(
+            session,
+            object_id=request_id,
+            fields=fields,
+        )
+        return updated_request
+
     async def create_request(
         self,
         session: AsyncSession,
@@ -48,6 +145,24 @@ class RequestRepository(BaseRepository[Request, RequestCreate, RequestUpdate]):
         """
         request = await self.create(session=session, object_in=request_object)
         return request
+
+    async def is_request_has_executor(
+        self, session: AsyncSession, *, request_id: int
+    ) -> Optional[bool]:
+        """
+        Check request has executor
+
+        Args:
+            session: Database session
+            request_id: Specific request id
+
+        Returns:
+            True if request has executor, else False or None if request is not found.
+        """
+        request = await self.get_request(session, request_id=request_id)
+        if not request:
+            return None
+        return True if request.executor_id else False
 
 
 request_repository = RequestRepository(Request)
