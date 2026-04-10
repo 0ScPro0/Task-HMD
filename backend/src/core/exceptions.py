29f7affddadce2c65a8a9d29c16ff6bcb0877f70
ScPro <muhamadievbogdan@gmai.com>
2026-04-10 23:48:06 +0500

    add CreateError exception

--- backend/src/core/exceptions.py
+++ backend/src/core/exceptions.py
@@ -3,6 +3,13 @@ from typing import Any, Dict, Optional
 from fastapi import HTTPException, status
 
 
+class CreateError(HTTPException):
+    def __init__(
+        self, detail: Any = None, headers: Optional[Dict[str, Any]] = None
+    ) -> None:
+        super().__init__(status.HTTP_400_BAD_REQUEST, detail, headers)
+
+
 class DuplicatedError(HTTPException):
     def __init__(
         self, detail: Any = None, headers: Optional[Dict[str, Any]] = None
