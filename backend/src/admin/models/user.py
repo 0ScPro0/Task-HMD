ce404022defeb185f5cc6c8c3f20b800a6f37733
ScPro <muhamadievbogdan@gmai.com>
2026-04-12 18:23:02 +0500

    update user admin model

--- backend/src/admin/models/user.py
+++ backend/src/admin/models/user.py
@@ -1,5 +1,8 @@
+from fastapi import Request
+from fastapi.datastructures import State
 from sqladmin import ModelView
 from database import User
+from core.security import hash_password
 
 
 class UserAdmin(ModelView, model=User):
@@ -24,3 +27,10 @@ class UserAdmin(ModelView, model=User):
         "refresh_token",
         "refresh_token_expires_at",
     ]
+
+    async def on_model_change(
+        self, data: dict, model: User, is_created: bool, request: Request[State]
+    ) -> None:
+        if is_created and "password_hash" in data and data["password_hash"]:
+            data["password_hash"] = hash_password(data["password_hash"])
+        return await super().on_model_change(data, model, is_created, request)
