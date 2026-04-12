af4431aef5ca9cf4cfcb9d4d1f410dc3ee5f90e1
ScPro <muhamadievbogdan@gmai.com>
2026-04-12 18:05:16 +0500

    update notification endpoints

--- backend/src/api/v1/endpoints/notifications.py
+++ backend/src/api/v1/endpoints/notifications.py
@@ -15,7 +15,7 @@ from utils.logger import log
 router = APIRouter(prefix="/notifications", tags=["notifications"])
 
 
-@router.get("/user", response_model=FullNotificationResponse)
+@router.get("/user", response_model=List[FullNotificationResponse])
 @log
 async def get_user_notifications(
     skip: int = 0,
@@ -25,18 +25,6 @@ async def get_user_notifications(
 ):
     return await notification_service.get_user_notifications(
         user=current_user, skip=skip, limit=limit, order_by=UserNotification.created_at
-    )
-
-
-@router.post("/create", response_model=FullNotificationResponse)
-@log
-async def create_notification(
-    notification: NotificationCreate,
-    current_user: User = Depends(get_current_user),
-    notification_service: NotificationService = Depends(get_notification_service),
-):
-    created_notification = await notification_service.create_notification(
-        notification=notification
     )
 
 
