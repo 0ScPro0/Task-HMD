04853f4430d5281a8bf77c10a864bd6fe184ea94
ScPro <muhamadievbogdan@gmai.com>
2026-04-28 02:56:55 +0500

    fix request service

--- backend/src/services/request.py
+++ backend/src/services/request.py
@@ -1,12 +1,12 @@
-from typing import Any, List, Optional
+from typing import Any, List, Optional, Union
 from sqlalchemy.ext.asyncio import AsyncSession
 
-from core.exceptions import NotFoundError, CreateError, PermissionDeniedError
-from database import Request, RequestStatus, User, UserRole
+from core.exceptions import NotFoundError, BadRequestError, PermissionDeniedError
+from database import Request, RequestStatus, RequestType, User, UserRole
 from repositories import RequestRepository
 from schemas.request import RequestCreate, RequestUpdate, RequestResponse
 from services.base import BaseService
-from utils.logger import log
+from utils.logger import log, logger
 
 
 class RequestService(
@@ -39,13 +39,16 @@ class RequestService(
         Returns:
             List of requests
         """
-        if user.role != UserRole.ADMIN:
+        if not self._is_admin(user):
             raise PermissionDeniedError("Not enough permissions")
+
         requests = await self.repository.get_many(
             self.session, skip=skip, limit=limit, order_by=order_by
         )
+
         if not requests:
             raise NotFoundError("Not found any requests")
+
         return [RequestResponse.model_validate(request) for request in requests]
 
     @log
@@ -72,9 +75,9 @@ class RequestService(
         return [RequestResponse.model_validate(request) for request in requests]
 
     @log
-    async def get_new_requests_by_role(
+    async def get_new_requests_by_executor_role(
         self,
-        role: UserRole,
+        executor: User,
         limit: Optional[int] = 100,
     ) -> Optional[List[RequestResponse]]:
         """
@@ -88,11 +91,11 @@ class RequestService(
         Returns:
             List of RequestResponse
         """
-        if role not in [UserRole.PLUMBER, UserRole.ELECTRICIAN]:
+        if not self._is_executor(executor) and not self._is_admin(executor):
             raise PermissionDeniedError("Not enough permissions")
 
         requests = await self.repository.get_requests_by_role(
-            self.session, role=role, limit=limit, status=RequestStatus.NEW
+            self.session, role=executor.role, limit=limit, status=RequestStatus.NEW
         )
         if not requests:
             return []
@@ -104,25 +107,33 @@ class RequestService(
         user: User,
         request_id: int,
     ) -> RequestResponse:
-        """
-        Get request by id
-
-        Args:
-            request_id: Request id
-
-        Returns:
-            RequestResponse
-        """
         request = await self.repository.get(self.session, id=request_id)
         if not request:
             raise NotFoundError("Request not found")
-        if (
-            user.role != UserRole.ADMIN
-            and request.owner_id != user.id
-            and request.executor_id != user.id
-        ):
-            raise PermissionDeniedError("Only admin, owner or executor can get request")
-        return RequestResponse.model_validate(request)
+
+        # Admin -> all
+        if self._is_admin(user):
+            return RequestResponse.model_validate(request)
+
+        # Executor (by role)
+        if self._is_executor(user):
+            # NEW request -> check if new
+            if request.status == RequestStatus.NEW:
+                if not self._is_request_type_executor_role(user, request):
+                    raise PermissionDeniedError("Not enough permissions")
+            # Not NEW → executor must be appointed
+            else:
+                if not self._is_request_executor(user, request):
+                    raise PermissionDeniedError("Not enough permissions")
+            return RequestResponse.model_validate(request)
+
+        # Resident → only his own requests
+        if self._is_resident(user):
+            if not self._is_request_owner(user, request):
+                raise PermissionDeniedError("Not enough permissions")
+            return RequestResponse.model_validate(request)
+
+        raise PermissionDeniedError("Not enough permissions")
 
     @log
     async def create_request(self, request: RequestCreate) -> Request:
@@ -139,12 +150,12 @@ class RequestService(
             self.session, request_object=request
         )
         if not created_request:
-            raise CreateError("Request was not created")
+            raise BadRequestError("Request was not created")
         return created_request
 
     @log
     async def update_request(
-        self, request_id: int, request: RequestUpdate
+        self, user: User, request_id: int, data: RequestUpdate
     ) -> RequestResponse:
         """
         Update request
@@ -156,16 +167,31 @@ class RequestService(
         Returns:
             RequestResponse
         """
-        updated_request = await self.repository.update(
-            self.session, update_object_id=request_id, object_in=request
+        current_request = await self.repository.get_request(
+            self.session, request_id=request_id
         )
-        if not updated_request:
+        if not current_request:
             raise NotFoundError("Request not found")
+
+        # Check user is admin
+        if not self._is_admin(user):
+            # Check user is executor of the request
+            if not self._is_request_executor(user, current_request):
+                # Check user is owner of the request
+                if not self._is_request_owner(user, current_request):
+                    raise PermissionDeniedError("Not enough permissions")
+
+        updated_request = await self.repository.update(
+            self.session, update_object_id=request_id, object_in=data
+        )
+        if not current_request:
+            raise NotFoundError("Request not found")
+
         return RequestResponse.model_validate(updated_request)
 
     @log
     async def executor_accept_request(
-        self, request_id: int, user: User, executor_id: int
+        self, request_id: int, executor: User
     ) -> RequestResponse:
         """
         Update request executor
@@ -177,37 +203,36 @@ class RequestService(
         Returns:
             RequestResponse
         """
-        request = await self.get_request(request_id=request_id, user=user)
+        request = await self.get_request(request_id=request_id, user=executor)
         if not request:
             raise NotFoundError("Request not found")
 
-        # Check user is admin or request executor with same role
-        if user.role != UserRole.ADMIN and request.type != user.role:
-            raise PermissionDeniedError(
-                "Only admin or executer with same role can update request executer"
-            )
+        # Check user is admin
+        if not self._is_admin(executor):
 
-        # Check executor does not respond to his own request
-        if request.owner_id == user.id:
-            raise PermissionDeniedError("Can not respond to your own requests")
+            # Check request.type == executor.role
+            if not self._is_request_type_executor_role(executor, request):
+                raise PermissionDeniedError("Not enough permissions")
+
+            # Check executor does not respond to his own request
+            if self._is_request_owner(executor, request):
+                raise PermissionDeniedError("Can not respond to your own requests")
 
         # Check if the status needs to be changed to in_progress
-        if request.status == RequestStatus.NEW:
+        if self._compare_request_status(request, RequestStatus.NEW):
             updated_request = await self.repository.update_request_executor_and_status(
                 self.session,
                 request_id=request_id,
-                executor_id=executor_id,
+                executor_id=executor.id,
                 status=RequestStatus.IN_PROGRESS,
             )
         else:
-            updated_request = await self.repository.update_request_executor(
-                self.session, request_id=request_id, executor_id=executor_id
-            )
+            raise PermissionDeniedError("Request is already accepted")
         return RequestResponse.model_validate(updated_request)
 
     @log
     async def update_request_status(
-        self, user_id: int, user_role: str, request_id: int, status: str
+        self, user: User, request_id: int, status: str
     ) -> RequestResponse:
         """
         Update request status
@@ -225,7 +250,7 @@ class RequestService(
             raise NotFoundError("Request not found")
 
         # Check user is admin or request executor
-        if user_role != UserRole.ADMIN and request.executor_id != user_id:
+        if not self._is_admin(user) and not self._is_request_executor(user, request):
             raise PermissionDeniedError(
                 "Only admin or request executer can change request status"
             )
@@ -236,9 +261,7 @@ class RequestService(
         return RequestResponse.model_validate(request)
 
     @log
-    async def delete_request(
-        self, user_id: int, user_role: str, request_id: int
-    ) -> bool:
+    async def delete_request(self, user: User, request_id: int) -> bool:
         """
         Delete request
 
@@ -258,17 +281,122 @@ class RequestService(
         if not request:
             raise NotFoundError("Request not found")
 
-        # Check user is admin, request owner or executor
-        if (
-            user_role != UserRole.ADMIN
-            and request.owner_id != user_id
-            and request.executor_id != user_id
-        ):
-            raise PermissionDeniedError(
-                "Only admin, worker or request owner can delete request"
-            )
+        # Check user is admin
+        if not self._is_admin(user):
+            # Check user is executor of the request
+            if not self._is_request_executor(user, request):
+                # Check user is owner of the request
+                if not self._is_request_owner(user, request):
+                    raise PermissionDeniedError("Not enough permissions")
 
         deleted_request = await self.repository.delete(self.session, id=request_id)
         if not deleted_request:
             raise NotFoundError("Request not found")
         return True
+
+    def _compare_request_status(
+        self, request: Request, status: Union[str, RequestStatus]
+    ) -> bool:
+        """
+        Compare request status with the submitted status
+
+        Args:
+            request: Request
+
+        Returns:
+            bool
+        """
+        if isinstance(status, RequestStatus):
+            status = status.value
+
+        return request.status.value == status
+
+    def _compare_user_role(self, user: User, role: Union[str, UserRole]) -> bool:
+        """
+        Compare user role with the submitted role
+
+        Args:
+            user: User
+
+        Returns:
+            bool
+        """
+        if isinstance(role, UserRole):
+            role = role.value
+
+        return user.role.value == role
+
+    def _is_request_owner(self, user: User, request: Request) -> bool:
+        """
+        Check if user is the request owner
+
+        Args:
+            request: Request
+
+        Returns:
+            bool
+        """
+        return request.owner_id == user.id
+
+    def _is_request_executor(self, executor: User, request: Request) -> bool:
+        """
+        Check if user is the request executor
+
+        Args:
+            request: Request
+
+        Returns:
+            bool
+        """
+        return request.executor_id == executor.id
+
+    def _is_resident(self, user: User) -> bool:
+        """
+        Check if user is the request owner
+
+        Args:
+            request: Request
+
+        Returns:
+            bool
+        """
+        return user.role.value == UserRole.RESIDENT.value
+
+    def _is_executor(self, executor: User) -> bool:
+        """
+        Check if user is the request executor
+
+        Args:
+            request: Request
+
+        Returns:
+            bool
+        """
+        return executor.role.value in [
+            UserRole.PLUMBER.value,
+            UserRole.ELECTRICIAN.value,
+        ]
+
+    def _is_admin(self, user: User) -> bool:
+        """
+        Check if user is the admin
+
+        Args:
+            request: Request
+
+        Returns:
+            bool
+        """
+        return user.role.value == UserRole.ADMIN.value
+
+    def _is_request_type_executor_role(self, executor: User, request: Request) -> bool:
+        """
+        Check if request type is the same as the executor role
+
+        Args:
+            request: Request
+
+        Returns:
+            bool
+        """
+        return request.type.value == executor.role.value
